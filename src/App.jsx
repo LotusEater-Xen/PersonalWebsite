56a58e25ba46f8b9ecc742dc5265f75b4f58d0df
LotusEater-Xen <lukasbi1007@gmail.com>
2026-05-24 21:18:40 +0200

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import penguin from './images/pengu.jpg';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
      <div className='bg-sky-50'>Pyengu is fat penguin</div>
-     
+    <img src={penguin} />
     </>
   )
 }
